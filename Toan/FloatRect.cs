82cbc20e0b501b6da6df721bd29eeba2a963746e
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-10-21 23:09:47 -0400

    Added FloatRect util struct

---
+++ Toan/FloatRect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+
+namespace Toan;
+
+public struct FloatRect
+{
+    public Vector2 Position { get; set; }
+    public Vector2 Size { get; set; }
+
+    public float Left   => Math.Min(Position.X, Position.X - Size.X);
+    public float Right  => Math.Max(Position.X, Position.X + Size.X);
+    public float Top    => Math.Min(Position.Y, Position.Y - Size.Y);
+    public float Bottom => Math.Max(Position.Y, Position.Y + Size.Y);
+
+    public FloatRect(Vector2 pos, Vector2 size)
+    {
+        Position = pos;
+        Size = size;
+    }
+
+    public FloatRect(float x, float y, float w, float h)
+    {
+        Position = new(x, y);
+        Size = new(w, h);
+    }
+
+    public FloatRect()
+    {
+        Position = Vector2.Zero;
+        Size = Vector2.Zero;
+    }
+}
