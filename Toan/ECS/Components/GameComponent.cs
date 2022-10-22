346246a2f34cd7b9eb45c4d55082810cd11315f7
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-10-21 23:59:10 -0400

    Implemented IWorldQueryable for GameComponent

--- Toan/ECS/Components/GameComponent.cs
+++ Toan/ECS/Components/GameComponent.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Toan.ECS.Components;
 
-public abstract class GameComponent
+public abstract class GameComponent : IWorldQueryable
 {
     public Guid Id { get; } = Guid.NewGuid();
+
+    public ISet<Guid> Reduce(ISet<Guid> entities, World world)
+    {
+        Type typeToFind = GetType();
+
+        return entities
+            .Where(entity => world.Entities[entity].Has(typeToFind))
+            .ToHashSet();
+    }
 }
