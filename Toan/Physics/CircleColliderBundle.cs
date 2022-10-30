a49383c61f154d536bce5d426d9aede24e49f1f0
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-10-29 23:25:57 -0400

    Added CircleColliderBundle

---
+++ Toan/Physics/CircleColliderBundle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+
+using Toan.ECS.Bundles;
+using Toan.ECS.Components;
+
+namespace Toan.Physics;
+public struct CircleColliderBundle : IBundle
+{
+    public required float Radius { get; init; }
+
+    public Vector2 Origin { get; init; }
+
+    public ulong Layer { get; init; }
+
+    public CollisionMask Mask { get; init; }
+
+    public void AddBundle(Guid entityId, ComponentRepository componentRepo)
+    {
+        IBundle.AddIfNew<Collider>(entityId, componentRepo, new()
+        {
+            Layer  = Layer,
+            Mask   = Mask,
+            Origin = Origin,
+            Shape  = ColliderShape.Circle,
+        });
+        IBundle.AddIfNew<CircleCollider>(entityId, componentRepo, new() { Radius = Radius });
+    }
+}
