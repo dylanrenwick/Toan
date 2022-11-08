6393a8b956d7f6f8462fa9aec6580cb4e2ac6b04
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-11-07 22:24:55 -0500

    Added Added and Changed types for using entity events in world queries

---
+++ Toan/ECS/Query/Added.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Toan.ECS.Components;
+
+namespace Toan.ECS.Query;
+
+/// <summary>
+/// Indicates that the query should only match entities added to the world since the last update.
+/// </summary>
+public class Added : IWorldQueryable
+{
+    public ISet<Guid> Reduce(World world, ISet<Guid> entities, ComponentRepository componentRepo)
+        => entities.Where(world.Events.WasAdded).ToHashSet();
+}
+
---
+++ Toan/ECS/Query/Changed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Toan.ECS.Components;
+
+namespace Toan.ECS.Query;
+
+/// <summary>
+/// Indicates that the query should only match entities whos components have changed since the last update.
+/// </summary>
+public class Changed : IWorldQueryable
+{
+    public ISet<Guid> Reduce(World world, ISet<Guid> entities, ComponentRepository componentRepo)
+        => entities.Where(world.Events.WasChanged).ToHashSet();
+}
