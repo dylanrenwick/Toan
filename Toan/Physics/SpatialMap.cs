bb8816302e64f65a52e70a88e1c33e98e67de8ef
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-11-07 23:33:22 -0500

    Implemented Spatial Hashmap in SpatialMap

--- Toan/Physics/SpatialMap.cs
+++ Toan/Physics/SpatialMap.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 
 using Microsoft.Xna.Framework;
 
+using Toan.ECS.Resources;
+
 namespace Toan.Physics;
 
-public class SpatialMap
+public class SpatialMap : Resource
 {
     private readonly Dictionary<Point, HashSet<Guid>> _spatialTable = new();
 
     public required int CellSize { get; init; }
 
-    public ISet<Guid> GetCloseColliders()
+    public IReadOnlySet<Guid> this[float x, float y]
     {
-        throw new NotImplementedException();
+        get
+        {
+            var cellPos = WorldToCellPos(new(x, y));
+            return _spatialTable.ContainsKey(cellPos)
+                ? _spatialTable[cellPos]
+                : new HashSet<Guid>();
+        }
     }
+
+    private Point WorldToCellPos(Vector2 worldPos)
+        => new(
+            x: (int)(worldPos.X / CellSize),
+            y: (int)(worldPos.Y / CellSize)
+        );
 }
