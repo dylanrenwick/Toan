1e125ce16357aa50fdc4451596f2b039b858705d
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-11-07 23:33:39 -0500

    Added SpatialMap as Resource in PhysicsPlugin

--- Toan/Physics/PhysicsPlugin.cs
+++ Toan/Physics/PhysicsPlugin.cs
@@ -4,8 +4,12 @@ namespace Toan.Physics;
 
 public class PhysicsPlugin : Plugin
 {
+    public int SpatialCellSize { get; init; } = 8;
+
     public override void Build(World world)
     {
+        world.AddResource(new SpatialMap { CellSize = SpatialCellSize });
+
         world.AddSystem<CircleCollisionSystem>();
     }
 }
