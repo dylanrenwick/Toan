3bf3f0bab4eb9ccab4e0c972dc86c1483eafe36d
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-11-08 00:42:29 -0500

    Added With, Height, Offset to FloatRect. Fixed Left and Top

--- Toan/FloatRect.cs
+++ Toan/FloatRect.cs
@@ -9,10 +9,13 @@ public struct FloatRect
     public Vector2 Position { get; set; }
     public Vector2 Size { get; set; }
 
-    public float Left   => Math.Min(Position.X, Position.X - Size.X);
+    public float Left   => Math.Min(Position.X, Position.X + Size.X);
     public float Right  => Math.Max(Position.X, Position.X + Size.X);
-    public float Top    => Math.Min(Position.Y, Position.Y - Size.Y);
+    public float Top    => Math.Min(Position.Y, Position.Y + Size.Y);
     public float Bottom => Math.Max(Position.Y, Position.Y + Size.Y);
+
+    public float Width  => Right - Left;
+    public float Height => Bottom - Top;
 
     public FloatRect(Vector2 pos, Vector2 size)
     {
@@ -31,4 +34,7 @@ public struct FloatRect
         Position = Vector2.Zero;
         Size = Vector2.Zero;
     }
+
+    public FloatRect Offset(Vector2 offset)
+        => new FloatRect(Position + offset, Size);
 }
