6b9dd1c0e3ddf53292d83d3b98a7fc962d3e442d
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-11-15 23:20:29 -0500

    Added RectCollider

---
+++ Toan/Physics/RectCollider.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;
+
+using Toan.ECS.Components;
+
+namespace Toan.Physics;
+
+public struct RectCollider : IComponent
+{
+    public required Vector2 Size { get; set; }
+}
