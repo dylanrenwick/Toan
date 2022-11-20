4e020cf862b3ef373311aa7eb82905cc266d9237
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-11-19 21:54:16 -0500

    Added Add to SpatialMap

--- Toan/Physics/SpatialMap.cs
+++ Toan/Physics/SpatialMap.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 
 using Microsoft.Xna.Framework;
-
+using Toan.ECS;
 using Toan.ECS.Resources;
 
 namespace Toan.Physics;
@@ -18,10 +18,31 @@ public class SpatialMap : Resource
         get
         {
             var cellPos = WorldToCellPos(new(x, y));
-            return _spatialTable.ContainsKey(cellPos)
-                ? _spatialTable[cellPos]
+            return _spatialTable.TryGetValue(cellPos, out var cell)
+                ? cell
                 : new HashSet<Guid>();
         }
+    }
+
+    public bool Add(Entity entity)
+        => Add(entity, ref entity.Get<Collider>());
+
+    public bool Add(Entity entity, ref Collider collider)
+        => Add(entity.Id, CollisionHelper.GetColliderBoundingBox(entity, ref collider));
+
+    public bool Add(Guid entityId, FloatRect boundingBox)
+    {
+        var cellBounds = CellBoundsFromRealBounds(boundingBox);
+        var cells = GetCellsInBounds(cellBounds);
+        bool success = false;
+
+        foreach (Point cell in cells)
+        {
+            // Only require any one success
+            success |= AddToCell(entityId, cell);
+        }
+
+        return success;
     }
 
     /// <summary>
@@ -33,32 +54,53 @@ public class SpatialMap : Resource
     /// <param name="height">Height of the bounding box</param>
     /// <returns></returns>
     public IReadOnlySet<Guid> GetPossibleCollisions(FloatRect boundingBox)
-    {
-        float cellOffsetX = boundingBox.Left % CellSize;
-        float cellOffsetY = boundingBox.Top % CellSize;
-
-        int cellOriginX = (int)Math.Floor(boundingBox.Left / CellSize);
-        int cellOriginY = (int)Math.Floor(boundingBox.Top / CellSize);
-
-        int cellBoundsX = (int)Math.Ceiling((cellOffsetX + boundingBox.Width) / CellSize);
-        int cellBoundsY = (int)Math.Ceiling((cellOffsetY + boundingBox.Height) / CellSize);
-
-        return GetEntitiesInBounds(new(cellOriginX, cellOriginY, cellBoundsX, cellBoundsY));
-    }
+        => GetEntitiesInBounds(CellBoundsFromRealBounds(boundingBox));
 
     public IReadOnlySet<Guid> GetEntitiesInBounds(Rectangle boundingBox)
     {
         HashSet<Guid> results = new();
 
+        foreach (Point cell in GetCellsInBounds(boundingBox))
+        {
+            results.UnionWith(this[cell.X, cell.Y]);
+        }
+
+        return results;
+    }
+
+    private bool AddToCell(Guid guid, Point cell)
+    {
+        if (!_spatialTable.ContainsKey(cell))
+            _spatialTable.Add(cell, new HashSet<Guid>());
+        return _spatialTable[cell].Add(guid);
+    }
+
+    private static IReadOnlySet<Point> GetCellsInBounds(Rectangle boundingBox)
+    {
+        HashSet<Point> results = new();
+
         for (int y = boundingBox.Y; y < boundingBox.Y + boundingBox.Height; y++)
         {
             for (int x = boundingBox.X; x < boundingBox.X + boundingBox.Width; x++)
             {
-                results.UnionWith(this[x, y]);
+                results.Add(new(x, y));
             }
         }
 
         return results;
+    }
+
+    private Rectangle CellBoundsFromRealBounds(FloatRect boundingBox)
+    {
+        float cellOffsetX = boundingBox.Left % CellSize;
+        float cellOffsetY = boundingBox.Top % CellSize;
+
+        return new(
+            x      : (int)Math.Floor(boundingBox.Left / CellSize),
+            y      : (int)Math.Floor(boundingBox.Top / CellSize),
+            width  : (int)Math.Ceiling((cellOffsetX + boundingBox.Width) / CellSize),
+            height : (int)Math.Ceiling((cellOffsetY + boundingBox.Height) / CellSize)
+        );
     }
 
     private Point WorldToCellPos(Vector2 worldPos)
