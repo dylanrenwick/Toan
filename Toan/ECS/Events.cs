9f154a1276c731279d39105a3cfce08b76b11644
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-11-29 02:43:00 -0500

    Added Removed event

--- Toan/ECS/Events.cs
+++ Toan/ECS/Events.cs
@@ -5,23 +5,33 @@ namespace Toan.ECS;
 
 public class Events : IEventsReader
 {
-    private HashSet<Guid> _added = new();
+    private HashSet<Guid> _added   = new();
     private HashSet<Guid> _changed = new();
+    private HashSet<Guid> _removed = new();
+
+    public IReadOnlySet<Guid> Added   => _added;
+    public IReadOnlySet<Guid> Changed => _changed;
+    public IReadOnlySet<Guid> Removed => _removed;
 
     public void AddEntity(Guid entityId)
         => _added.Add(entityId);
     public void ChangeEntity(Guid entityId)
         => _changed.Add(entityId);
+    public void RemoveEntity(Guid entityId)
+        => _removed.Add(entityId);
 
     public bool WasAdded(Guid entityId)
         => _added.Contains(entityId);
     public bool WasChanged(Guid entityId)
         => _changed.Contains(entityId);
+    public bool WasRemoved(Guid entityId)
+        => _removed.Contains(entityId);
 
     public void Clear()
     {
         _added.Clear();
         _changed.Clear();
+        _removed.Clear();
     }
 }
 
--- Toan/ECS/IEventsReader.cs
+++ Toan/ECS/IEventsReader.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Toan.ECS;
 
 public interface IEventsReader
 {
+    public IReadOnlySet<Guid> Added { get; }
+    public IReadOnlySet<Guid> Changed { get; }
+    public IReadOnlySet<Guid> Removed { get; }
+
     public bool WasAdded(Guid entityId);
     public bool WasChanged(Guid entityId);
+    public bool WasRemoved(Guid entityId);
 }
