ec965e17ef5a3e9faed4f28a7615b47f7b376a0a
Dylan Renwick <skidsirongenesisdev@gmail.com>
2023-01-01 01:34:27 -0500

    Removed unused imports and marked Util class static

--- Toan/Util/Util.cs
+++ Toan/Util/Util.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Toan;
 
-public class Util
+public static class Util
 {
     public static void EnsureLength<T>(ref T[] arr, int minLength, int maxLength = int.MaxValue)
     {
