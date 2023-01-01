a32cdb85b910329f26d1dcd03486c4708c34bfa6
Dylan Renwick <skidsirongenesisdev@gmail.com>
2023-01-01 01:34:37 -0500

    Added generic TreeNode class to Util

---
+++ Toan/Util/TreeNode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Toan;
+
+public class TreeNode<T>
+    where T : notnull
+{
+    public TreeNode<T>? Parent { get; set; }
+    public readonly HashSet<TreeNode<T>> Children = new();
+
+    public required T Value { get; set; }
+}
