e78a4472885b92cae551776b60b79a10280b9efe
Dylan Renwick <skidsirongenesisdev@gmail.com>
2023-01-06 21:57:47 -0500

    Refactored SystemRepository.ExecuteInPriorityOrder to call the action once and pass the whole system set

--- Toan/ECS/Systems/SystemRepository.cs
+++ Toan/ECS/Systems/SystemRepository.cs
@@ -21,47 +21,54 @@ public class SystemRepository
     public void Add(
         SystemInfo system,
         SystemExecutionPriority priority = SystemExecutionPriority.Standard
-    )
-    => _systems[priority].Add(system);
+    ) {
+        _systems[priority].Add(system);
+    }
 
     public void Update(World world, GameTime gameTime)
     {
         var updateParams = new object[] { world, gameTime };
 
-        ExecuteInPriorityOrder(system =>
-            system.UpdateSystem?.Invoke(system.System, updateParams)
-        );
+        ExecuteInPriorityOrder(systems => {
+            foreach (var system in systems)
+            {
+                system.UpdateSystem?.Invoke(system.System, updateParams);
+            }
+        });
     }
 
     public void Render(World world, Renderer renderer, GameTime gameTime)
     {
         var renderParams = new object[] { world, renderer, gameTime };
 
-        ExecuteInPriorityOrder(system =>
-            system.RenderSystem?.Invoke(system.System, renderParams)
-        );
+        ExecuteInPriorityOrder(systems => {
+            foreach (var system in systems)
+            {
+                system.RenderSystem?.Invoke(system.System, renderParams);
+            }
+        });
     }
 
 	public void UpdateComponents(World world)
     {
-        ExecuteInPriorityOrder(system => {
-            if (system.EntitySystem == null || system.EntityQuery == null)
-                return;
-            var query = (IWorldQuery)system.EntityQuery.GetValue(system.System)!;
-            var entities = query.GetEntities(world);
-            system.EntitySystem?.Invoke(system.System, new object[] { world, entities });
+        ExecuteInPriorityOrder(systems => {
+            foreach (var system in systems)
+            {
+                if (system.EntitySystem == null || system.EntityQuery == null)
+                    return;
+                var query = (IWorldQuery)system.EntityQuery.GetValue(system.System)!;
+                var entities = query.GetEntities(world);
+                system.EntitySystem?.Invoke(system.System, new object[] { world, entities });
+            }
         });
     }
 
-    private void ExecuteInPriorityOrder(Action<SystemInfo> action)
+    private void ExecuteInPriorityOrder(Action<ISet<SystemInfo>> action)
     {
         var execPriorities = Enum.GetValues<SystemExecutionPriority>();
         for(int i = 0; i < execPriorities.Length; i++)
         {
-            foreach (SystemInfo system in _systems[execPriorities[i]])
-            {
-                action(system);
-            }
+            action(_systems[execPriorities[i]]);
         }
     }
 }
