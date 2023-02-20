6c1a74fae667f7bf1dea5b7858123792d4a94809
Dylan Renwick <skidsirongenesisdev@gmail.com>
2023-02-20 00:16:50 -0500

    Added Diagnostics stats to DebugLogSystem

--- Toan/Debug/DebugLogSystem.cs
+++ Toan/Debug/DebugLogSystem.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Text;
 using Microsoft.Xna.Framework;
 
 using Toan.ECS;
@@ -7,11 +7,15 @@ using Toan.ECS.Components;
 using Toan.ECS.Query;
 using Toan.ECS.Resources;
 using Toan.ECS.Systems;
+using Toan.Input;
+using Toan.Rendering;
 
 namespace Toan.Debug;
 
 public class DebugLogSystem : EntityUpdateSystem
 {
+    private const bool SHOW_LOG = false;
+
     public override WorldQuery<DebugLog, Text> Archetype => new();
 
     protected override void UpdateEntity(Entity entity, GameTime gameTime)
@@ -25,17 +29,38 @@ public class DebugLogSystem : EntityUpdateSystem
             return;
         }
 
-        var debugLog = entity.Get<DebugLog>();
-        var text     = entity.Get<Text>();
+        var text = entity.Get<Text>();
 
         if (!visible)
             AddVisible(entity);
 
-        Guid logId = debugLog.LogResourceID;
-        TextLog log = entity.World.Resource<TextLog>(logId);
+        // TODO: Automate this toggle
+        if (SHOW_LOG)
+        {
+            var debugLog = entity.Get<DebugLog>();
+            Guid logId = debugLog.LogResourceID;
+            TextLog log = entity.World.Resource<TextLog>(logId);
 
-        text.Content = log.GetEntries(debugLog.EntryCount);
+            text.Content = log.GetEntries(debugLog.EntryCount);
+        }
+        else
+        {
+            text.Content = GetDiagnosticStats(entity.World);
+        }
+
         entity.With(text);
+    }
+
+    protected string GetDiagnosticStats(World world)
+    {
+        InputState input = world.Resource<InputState>();
+        Camera camera = MainCamera.MainCameraEntity;
+
+        StringBuilder sb = new();
+        sb.AppendLine($"Mouse Pos: {input.MousePosition}");
+        sb.AppendLine($"World Pos: {camera.ScreenToWorld(input.MousePosition.ToVector2())}");
+
+        return sb.ToString();
     }
 
     protected void RemoveVisible(Entity entity)
