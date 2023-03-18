2b432aaf24532bdd5f6994707ae7aeed958ea25c
Dylan Renwick <skidsirongenesisdev@gmail.com>
2023-03-18 05:18:34 -0400

    Implemented VirtualEntity Id swapping for multiple makes

--- Toan/ECS/VirtualEntity.cs
+++ Toan/ECS/VirtualEntity.cs
@@ -7,6 +7,15 @@ namespace Toan.ECS;
 internal class VirtualEntity : BaseEntity
 {
     private Dictionary<Type, ValueType> _components = new();
+    private bool _isMade = false;
+
+    private Guid _id = Guid.Empty;
+
+    public override required Guid Id
+    {
+        get => _id;
+        init => _id = value;
+    }
 
     public override T Get<T>()
         => (T)_components[typeof(T)];
@@ -25,5 +34,30 @@ internal class VirtualEntity : BaseEntity
     {
         _components.Remove(typeof(T));
         return this;
+    }
+    public override Guid Make()
+    {
+        do
+        {
+            if (World.HasEntity(_id))
+            {
+                Entity worldEntity = World.Entity(_id);
+                if (worldEntity.Count() >= 1)
+                {
+                    _id = Guid.NewGuid();
+                    continue;
+                }
+            }
+            break;
+        } while (true);
+
+        _isMade = true;
+        return base.Make();
+    }
+
+    ~VirtualEntity()
+    {
+        if (!_isMade)
+            World.Log.Warn($"Entity {Id} was not made before being destroyed");
     }
 }
