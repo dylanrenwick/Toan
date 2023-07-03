1421a8da294cc44890509d6539105f7d06a29c16
Dylan Renwick <skidsirongenesisdev@gmail.com>
2023-07-03 01:06:04 -0400

    Added DynamicBundle

---
+++ Toan/ECS/Bundles/DynamicBundle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Toan.ECS.Components;
+
+namespace Toan.ECS.Bundles;
+
+public class DynamicBundle : IBundle
+{
+    public HashSet<IComponent> Components { get; init; } = new();
+
+    public HashSet<IComponent> FlattenBundle()
+        => Components;
+
+    public void AddBundle(Entity entity)
+    {
+        foreach (var component in Components)
+        {
+            entity.With(component);
+        }
+    }
+
+    public static DynamicBundleBuilder Build()
+        => new();
+
+    public readonly struct DynamicBundleBuilder
+    {
+        public HashSet<IComponent> Components { get; init; } = new();
+
+        public DynamicBundleBuilder() { }
+
+        public DynamicBundleBuilder With(IComponent component)
+        {
+            Components.Add(component);
+            return this;
+        }
+
+        public DynamicBundleBuilder WithBundle(IBundle bundle)
+        {
+            Components.UnionWith(bundle.FlattenBundle());
+            return this;
+        }
+
+        public DynamicBundle Bundle()
+            => new() { Components = Components };
+    }
+}
