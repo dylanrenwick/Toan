8c00862fa7f0397610b6acc702e311590fa5c0a4
Dylan Renwick <skidsirongenesisdev@gmail.com>
2023-08-12 22:56:12 -0400

    Updated DrawTextureCall to use RoundToPoint

--- Toan/Rendering/DrawTextureCall.cs
+++ Toan/Rendering/DrawTextureCall.cs
@@ -8,18 +8,22 @@ public record DrawTextureCall : DrawCall
     public required Texture2D Texture { get; init; }
 
     private Point TextureSize => new(Texture.Width, Texture.Height);
-    public Vector2? Size { get; init; }
+    private Vector2? _size;
+    public Vector2 Size {
+        get => _size ?? TextureSize.ToVector2();
+        init => _size = value;
+    }
 
     public Rectangle DestRect => new (
-        location: Position.ToPoint(),
-        size: ((Size ?? TextureSize.ToVector2()) * Scale).ToPoint()
+        location : MathUtil.RoundToPoint(Position),
+        size     : MathUtil.RoundToPoint(Size * Scale)
     );
 
     public Vector2 SourcePosition { get; init; } = Vector2.Zero;
     public Vector2? SourceSize { get; init; }
 
     public Rectangle SourceRect => new(
-        location: SourcePosition.ToPoint(),
-        size: SourceSize?.ToPoint() ?? TextureSize
+        location : SourcePosition.ToPoint(),
+        size     : SourceSize?.ToPoint() ?? TextureSize
     );
 }
